4a224f3f0b1cb9231ae1e17c48131a4792cc1f10
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-24 19:20:16 -0600

    𝑇𝑂𝐾𝐼𝑇𝑂-𝐵𝑂𝑇-𝑀𝐷

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Gata\nBot\nMD', {
+say('TOKITO\nBOT\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)`, {
+say(`Project Author:\nWilsmac (@INDEFINIDO)\n\nColaboradores:\nGataNina-Li (@gata_dios)`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']});
