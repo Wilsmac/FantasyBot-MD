177469a645246c1bd05f637eee0d32992ab1f14b
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-24 18:55:40 -0600

    𝑇𝑂𝐾𝐼𝑇𝑂-𝐵𝑂𝑇-𝑀𝐷

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
   
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="GataBot-MD"  
-BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
+BOT_DIR="TOKITOBOT-MD"  
+BOT_REPO="https://github.com/Wilsmac/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
