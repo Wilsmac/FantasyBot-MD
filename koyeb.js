f8ae1127b456d0515cb5ba2e982a1aca39410671
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-24 19:16:29 -0600

    𝑇𝑂𝐾𝐼𝑇𝑂-𝐵𝑂𝑇-𝑀𝐷

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone https://github.com/Wilsmac/TOKITOBOT-MD
 
-WORKDIR /root/GataBot-MD
+WORKDIR /root/TOKITOBOT-MD
 
-COPY ./root/GataBot-MD
+COPY ./root/TOKITOBOT-MD
 
 RUN yarn install
 
