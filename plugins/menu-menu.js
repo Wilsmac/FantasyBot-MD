9eaed05054bd5e29ee35aee8e0e09baf9e50a423
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-07 11:02:50 -0600

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -635,7 +635,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)}}
 
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
-//handler.register = true
+handler.register = true
 export default handler
 
 const more = String.fromCharCode(8206)
