20db70920269deb15dff255a48f848465801c919
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-07 22:25:34 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -1,5 +1,5 @@
-/* ///
-import { sticker } from '../lib/sticker.js'
+
+/*import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 
@@ -33,4 +33,4 @@ return !0
 export default handler
 
 const isUrl = (text) => {
-/// return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))} *\
+ return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}*/
