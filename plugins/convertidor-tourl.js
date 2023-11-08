6aafc7058484b1ecda712bc02edb386a02f805dc
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-08 11:56:31 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,25 +1,15 @@
-
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
-let handler = async (m) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
-let name = await conn.getName(who)
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw `${mg} 𝑅𝐸𝑆𝑃𝑂𝑁𝐷𝐴 𝐴 𝑈𝑁𝐴 𝐼𝑀𝐴𝑁𝐸𝑁 𝑂 𝑉𝐼𝐷𝐸𝑂`
-let media = await q.download()
-let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-let link = await (isTele ? uploadImage : uploadFile)(media)
-let caption = `𝐸𝑁𝐿𝐴𝐶𝐸:\n${link}\n𝑇𝐴𝑀𝐴𝑁̃𝑂: ${media.length}\n𝐸𝑋𝑃𝐼𝑅𝐴: ${isTele ? '𝑁𝑂 𝐸𝑋𝑃𝐼𝑅𝐴 :𝐷𝐸𝑆𝐶𝑂𝑁𝑂𝐶𝐼𝐷𝑂'}\n𝐴𝐶𝑂𝑅𝑇𝐴𝐷𝑂: ${await shortUrl(link)}`
-conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
-handler.help = ['tourl']
-handler.tags = ['herramientas']
-handler.command = /^(tourl|upload)$/i
-export default handler
-
-async function shortUrl(url) {
-let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-return await res.text()
-}
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙰 𝙴𝙽𝙻𝙰𝙲𝙴*';
+  const media = await q.download();
+  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const link = await (isTele ? uploadImage : uploadFile)(media);
+  m.reply(`*𝙴𝙽𝙻𝙰𝙲𝙴 𝙰 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾:* ${link}`);
+};
+handler.help = ['tourl <reply image>'];
+handler.tags = ['sticker'];
+handler.command = /^(upload|tourl)$/i;
+export default handler;
