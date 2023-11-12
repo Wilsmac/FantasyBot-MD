2d33d597f64d9a0820fb67ebbcc023cee4567d14
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-12 16:19:11 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/info-contacto.js
+++ plugins/info-contacto.js
@@ -32,7 +32,7 @@ await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}} 
-handler.command = /^(contactos?|contacts?)$/i //['contacto', 'contact']  
+handler.command = /^(contacto?|contacts?)$/i //['contacto', 'contact']  
 
 export default handler
 
