437b0c91aa13d40b7feaa360e2ee39afc51161dd
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-12 23:20:49 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,11 +2,11 @@ let handler = m => m
 handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
 let user = global.db.data.users[m.sender]
 if (user.afk > -1) {
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}✴️ *A F K* ✴️
+await conn.reply(m.chat, `${lenguajeCD['smsAvisoEG']()}✴️ *A F K* ✴️
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
+*@${m.sender.split("@")[0]}* ${lenguajeCD['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
 
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
+${lenguajeCD['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
 user.afk = -1
 user.afkReason = ''
 }
@@ -19,11 +19,11 @@ let afkTime = user.afk
 if (!afkTime || afkTime < 0)
 continue
 let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
+await conn.reply(m.chat, `${lenguajeCD['smsAvisoAG']()}✴️ *A F K* ✴️
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
+😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeCD['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeCD['smsAfkM6']()}`}
 
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
+${lenguajeCD['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
 }
 return true
 }
