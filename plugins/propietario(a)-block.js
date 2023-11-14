b42bb35e1204f3ae2eb8d4e32ce4e271b54f0934
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-14 14:50:01 -0600

    𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄

--- plugins/propietario(a)-block.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let blocktext = `*ETIQUTE A LA PERSONA O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE bloqueo*\n\n*EJEMPLO: ${usedPrefix + command} @${global.owner[0][0]}*`
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(blocktext, m.chat, { mentions: conn.parseMention(blocktext)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await sock.updateBlockStatus("xyz@s.whatsapp.net", "block")}
-handler.command = /^(block|bloquear|block2)$/i
-handler.group = true
-handler.chat = true
