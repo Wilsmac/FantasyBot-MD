f26907ea89d06d41d9024d924df2022c792c8173
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-16 16:40:37 -0600

    𝑊𝑖𝑙𝑚𝑒𝑟 𝑜𝑓𝑐

--- plugins/propietario(a) -actualizacion.js
+++ plugins/propietario(a) -actualizacion.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
-const owner = 'GataNina-Li';
-const repo = 'GataBot-MD';
+const owner = 'Wilsmac';
+const repo = 'FantasyBot-MD';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   async function checkRepoUpdates() {
     try {
