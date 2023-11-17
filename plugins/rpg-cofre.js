6f66130a84566af24013bfbb09cf6a0769eb6606
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-11-16 19:03:20 -0600

    𝑊𝑖𝑙𝑚𝑒𝑟 𝑜𝑓𝑐

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -1,6 +1,6 @@
 let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 86400000 // 36000000 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastcofre < 86400000) throw `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝘾𝙊𝙁𝙍𝙀 ⚗️\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍\n\n𝙔𝙊𝙐 𝘼𝙇𝙍𝙀𝘼𝘿𝙔 𝘾𝙇𝘼𝙄𝙈𝙀𝘿 𝙔𝙊𝙐𝙍 𝘾𝙊𝙁𝙁𝙀𝙍 ⚗️\n𝘾𝙊𝙈𝙀 𝘽𝘼𝘾𝙆 𝙄𝙉 *${msToTime(time - new Date())}* 𝙏𝙊 𝘾𝙇𝘼𝙄𝙈 𝘼𝙂𝘼𝙄𝙉`
+if (new Date - global.db.data.users[m.sender].lastcofre < 86400000) throw `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝘾𝙊𝙁𝙍𝙀 ⚗️\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍`
 
 let img = 'https://img.freepik.com/vector-gratis/cofre-monedas-oro-piedras-preciosas-cristales-trofeo_107791-7769.jpg?w=2000'
 let dia = Math.floor(Math.random() * 30)
@@ -16,13 +16,13 @@ let expp = Math.floor(Math.random() * 5000)
 let texto = `
 ╭━━🎉━🎉━🎉━━⬣
 ┃✨ 𝙊𝘽𝙏𝙄𝙀𝙉𝙀𝙎 𝙐𝙉 𝘾𝙊𝙁𝙍𝙀!!
-┃✨ 𝙔𝙊𝙐 𝙂𝙀𝙏 𝘼 𝘾𝙊𝙁𝙁𝙀𝙍!!
+┃
 ┃ ┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃⚗️ *${dia} Diamantes* 💎
 ┃⚗️ *${tok} Tokens* 🪙
-┃⚗️ *${gata} GataCoins* 🐈
+┃⚗️ *${gata} FantasyCoins* 🌹
 ┃⚗️ *${expp} Exp* ⚡
-╰━━〔 𓃠 *${vs}* 〕━━⬣`
+╰━━━━❰･${vs}･❱━━━━`
 
 const fkontak = {
 	"key": {
