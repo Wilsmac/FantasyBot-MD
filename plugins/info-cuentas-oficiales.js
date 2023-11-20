2c09361850688e032624b1fd7f1ce8595fc0f356
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-11-19 18:00:19 -0600

    [♥︎𝐿] 𝑌 [☆𝑊] 𝑋 𝗦𝗜𝗘𝗠𝗣𝗥𝗘

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -20,6 +20,6 @@ await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}*/
 
-handler.command = /^cuentasoficiales|wilmerig|cuentascd|cuentacd|$/i
+handler.command = /^cuentasoficiales|wilmerig|cuentascd|cuentacd|cuentaswl$/i
 handler.exp = 35
 export default handler
