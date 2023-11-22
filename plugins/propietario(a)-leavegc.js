0ed2510de63c291a987e0ebea4dadbd4d9658761
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-11-22 06:33:30 -0600

    [♥︎𝐿] 𝑌 [☆𝑊] 𝑋 𝗦𝗜𝗘𝗠𝗣𝗥𝗘

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `*${wm} 𝘼𝘽𝘼𝙉𝘿𝙊𝙉𝘼 𝙀𝙇 𝙂𝙍𝙐𝙋𝙊, 𝙁𝙐𝙀 𝙂𝙀𝙉𝙄𝘼𝙇 𝙀𝙎𝙏𝘼𝙍 𝘼𝙌𝙐𝙄 👋*`) 
+await conn.reply(id, `*${packname} 𝑨𝑩𝑨𝑵𝑫𝑶𝑵𝑨 𝑬𝑳 𝑮𝑹𝑼𝑷𝑶, 𝑭𝑼𝑬 𝑭𝑬𝑵𝑰𝑨𝑳 𝑬𝑺𝑻𝑨𝑹 𝑨𝑸𝑼𝑼 𝑯𝑨𝑺𝑻𝑨 𝑷𝑹𝑶𝑵𝑻𝑶 😉*`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
