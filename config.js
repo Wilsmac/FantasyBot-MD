b5a9bd642bc5d77814b21920f5b5e6e373b13f03
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-11-27 20:35:23 -0600

    [♥︎𝐿] 𝑌 [☆𝑊] 𝑋 𝗦𝗜𝗘𝗠𝗣𝗥𝗘

--- config.js
+++ config.js
@@ -215,29 +215,29 @@ global.imagen13 = fs.readFileSync("./media/menus/img12.jpg")
 
 //━━━━━━━━━━━━━━ img ━━━━━━━━━
 
-global.img = 'https://i.imgur.com/IXlUwTW.jpg'
-global.img2 = 'https://i.imgur.com/EXTbyyn.jpg'
+global.img = 'https://telegra.ph/file/6114942024c7658478830.jpg'
+global.img2 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
 
 global.img3 = 'https://i.imgur.com/oUAGYc2.jpg' //prem
 global.img4 = 'https://i.imgur.com/i0pccuo.jpg' //prem
 
-global.img5 = 'https://i.imgur.com/iL1snRx.jpeg'
-global.img6 = 'https://i.imgur.com/cYFgSKv.jpeg'
-global.img7 = 'https://i.imgur.com/JqL3h2V.jpeg'
-global.img8 = 'https://i.imgur.com/PCujt1s.jpeg'
-global.img9 = 'https://i.imgur.com/xfUEdDb.jpeg'
+global.img5 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
+global.img6 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
+global.img7 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
+global.img8 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
+global.img9 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
 
-global.img10 = 'https://i.imgur.com/DvHoMc3.jpg'
-global.img11 = 'https://i.imgur.com/5Q1MqGD.jpg'
-global.img12 = 'https://i.imgur.com/vWnsjh8.jpg'
+global.img10 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
+global.img11 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
+global.img12 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
 global.img13 = 'https://i.imgur.com/pCfFOgw.jpeg'
-global.img14 = 'https://i.imgur.com/knBDWRA.jpeg'
-global.img15 = 'https://i.imgur.com/QrkkKx7.jpeg'
+global.img14 = 'https://telegra.ph/file/418da430e42952c392751.jpg'
+global.img15 = 'hhttps://telegra.ph/file/418da430e42952c392751.jpg'
 
-global.img16 = 'https://i.imgur.com/11MRjo4.jpeg' //+18
+global.img16 = 'https://telegra.ph/file/6114942024c7658478830.jpg' //+18
 
-global.img17 = 'https://i.imgur.com/JpYfcH0.jpeg'
-global.img18 = 'https://i.imgur.com/9yLH4W4.jpeg'
+global.img17 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
+global.img18 = 'https://telegra.ph/file/6114942024c7658478830.jpg'
 
 global.logogit = 'https://tinyurl.com/2qvl9vgs'
 
