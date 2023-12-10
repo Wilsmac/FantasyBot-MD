8e6fcfc91c9f81c2f295f259467131bb3318ccb6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-12-10 13:16:17 -0600

    Update chatbot.j

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -34,7 +34,22 @@ if (/^buenos días|buenos dias|hola, buenos días$/i.test(m.text) ) { //sin pref
   
 
  
+if (/^Hola|dias|nose$/i.test(m.text) ) { //sin prefijo 
+    let teks = `${pickRandom([`*"Comenzar el día orando y dando gracias a dios es a lo que yo llamo un buen amanecer."*`,
+                `*"Mientras empieces el día sonriendo, estás asegurando que será un buen día."*`, 
+                `**Aunque la lluvia haga un día muy oscuro, tu tienes el sol por dentro."*`,
+                `*"Gracias por hacerme tan especial, gracias por estar conmigo, que la bendición y el amor te acompañen en este nuevo día."*`,
+                `*"La vida y el tiempo son los mejores maestros. La vida nos enseña a aprovechar el tiempo y el tiempo nos enseña a valorar la vida."*`, 
+                `*"No te limites en tu andar guiándote solamente por el camino de los demás, sé emprendedor y ve por donde no hay un camino para comenzar a dejar tus huellas, Buenos Dias"*`,
+                `*"Buenos días a toda esa gente bonita que me rodea… bueno y a la fea también"*`,
+                `*"Nacemos de nuevo con cada amanecer. Que este día sea el más importante de tu vida. Buenos días."*`,
+                `*"El brillo de cada día, no depende del sol sino de tu sonrisa que sale del corazón. Que tengas un feliz día."*`,
+                `*"Lo mejor de empezar un nuevo día es que voy a poder ver tu sonrisa, buenos día."*`,
+                `*"El momento para todo es el ahora. ¡Despiértate y ten un gran día!"*`,
+                `*"No te limites en tu andar guiándote solamente por el camino de los demás, sé emprendedor y ve por donde no hay un camino para comenzar a dejar tus huellas, Buenos Dias"*`])}
+                `.trim() conn.reply(m.chat, fkontak, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
+}
  
 /*if (/^Mande porno|porno|paja$/i.test(m.text) ) { //sin prefijo 
     let teks = `
